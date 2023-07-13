936e812ae699c265237cb3dc0d36edab699d172e
asmo <alvarado.1999.joe@gmail.com>
2023-07-13 04:09:13 -0500

    Practice writing functions, reading tests and test errors

---
+++ index.js
@@ -0,0 +1,46 @@
+const { spyOn } = require("expect");
+
+function shout(string) {
+    return string.toUpperCase(
+        
+    );
+}
+
+"Hello!".toUpperCase();
+
+function whisper(string) {
+    return string.toLowerCase(
+
+    );
+}
+
+"Hello!".toLowerCase();
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+console.log("hello!".toUpperCase);
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+console.log("HELLO!".toLowerCase);
+
+function sayHiToHeadphonedRoommate(string) {
+    var cantUnswer = "I can\'t hear you!";
+    var yesUnswer = "YES INDEED!";
+    var lovUnswer = "I would love to!"
+    if (string.toLowerCase(string) === string) {
+        return cantUnswer;
+    }
+    else if (string.toUpperCase(string) === string) {
+        return yesUnswer;
+    }
+    else if ("Let\'s have dinner together!") {
+        return lovUnswer
+    }
+}
+
+console.log("HI!")
